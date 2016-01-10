bca021110ec07fc58d41ed1cd7a7a082dcb2b1df
Wojtek Dmyszewicz <wdmy85@gmail.com>
2016-01-10 04:06:11 +0100

    completed project

--- Pirate Fleet/ControlCenter.swift
+++ Pirate Fleet/ControlCenter.swift
@@ -17,18 +17,57 @@ struct Ship {
     let isVertical: Bool
 }
 
+struct Mine: _Mine_ {
+    let location: GridLocation
+    let explosionText : String
+}
+
 class ControlCenter {
-    
+
     func addShipsAndMines(human: Human) {
-        <#WRITE YOUR CODE HERE!#>
+        let mediumShip1 = Ship(length: 3, location: GridLocation(x:0, y:0),
+                isVertical: false)
+
+        let mediumShip2 = Ship(length: 3, location: GridLocation(x:0, y:2),
+                isVertical: false)
+
+        let smallShip1 = Ship(length: 2, location: GridLocation(x:0, y:4),
+                isVertical: false)
+
+        let largeShip1 = Ship(length: 4, location: GridLocation(x:0, y:6),
+                isVertical: false)
+
+        let extraLargeShip1 = Ship(length: 5, location: GridLocation(x:6, y:0),
+                isVertical: true)
+
+        let mine1 = Mine(location: GridLocation(x:5, y:0), explosionText: "you got wasted")
+        let mine2 = Mine(location: GridLocation(x:5, y:3), explosionText: "oeps, your blown up")
+
+        human.addShipToGrid(mediumShip1)
+        human.addShipToGrid(mediumShip2)
+        human.addShipToGrid(smallShip1)
+        human.addShipToGrid(largeShip1)
+        human.addShipToGrid(extraLargeShip1)
+
+        human.addMineToGrid(mine1)
+        human.addMineToGrid(mine2)
     }
-    
+
     func calculateFinalScore(gameStats: GameStats) -> Int {
-        
+
         var finalScore: Int
-        
-        finalScore = 0                
-        
+
+        let maxShips = 5
+        let enemyShipsSunk = maxShips - gameStats.enemyShipsRemaining
+        let humanSchipsRemaining = maxShips - gameStats.humanShipsSunk
+        let numberOfGuesses = gameStats.numberOfHitsOnEnemy + gameStats.numberOfMissesByHuman
+
+        finalScore = (enemyShipsSunk * gameStats.sinkBonus) +
+                (humanSchipsRemaining * gameStats.shipBonus) -
+                (numberOfGuesses * gameStats.guessPenalty)
+
+        print("the value of final score is \(finalScore)")
+
         return finalScore
     }
 }
